b4e1686c035b65d5f17a11527ac627533e6fd381
shalevshir <shalevshir@gmail.com>
2025-11-23 16:52:32 +0700

    feat: export VWAP analysis functions from vwap module
    - Export calculate_htf_vwap (HTF VWAP calculation)
    - Export validate_vwap_trend (trend confirmation)
    - Export score_fvg_alignment (FVG interaction)
    - Update __all__ list for complete public API
    - Resolve merge conflict maintaining all exports

--- rule_engine/htf/vwap/__init__.py
+++ rule_engine/htf/vwap/__init__.py
@@ -7,6 +7,8 @@ This package handles:
 """
 
 from rule_engine.htf.vwap.calculator import calculate_htf_vwap
+from rule_engine.htf.vwap.fvg import score_fvg_alignment
+from rule_engine.htf.vwap.trend import validate_vwap_trend
 
-__all__ = ["calculate_htf_vwap"]
+__all__ = ["calculate_htf_vwap", "score_fvg_alignment", "validate_vwap_trend"]
 
